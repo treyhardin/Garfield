19e46dbaf259492e377d3745a9f2c7cd16883aa0
Trey Hardin <treyhardindesign@gmail.com>
2022-03-22 10:57:28 -0400

    Site Links

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -4,7 +4,7 @@ export default function About() {
 
     return (
         <section data-scroll-section data-scroll-speed="1" className='about-section'>
-            <h1 data-scroll className='about-text'>An experiment by Trey Hardin, a Creative Director &amp; Designer based in Brooklyn, New York.</h1>
+            <h1 data-scroll className='about-text'>An experiment by <a href="treyhardin.com" target="_blank">Trey Hardin</a>, a Creative Director &amp; Designer based in Brooklyn, New York.</h1>
         </section>
     )
 }
